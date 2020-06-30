e294c50c8bd7d8bc8262f8b791821f17a491c285
Xanthus Wong <lonelynok@gmail.com>
2020-06-30 22:28:07 +0800

    - first init

---
+++ WebApplication2/TestResultFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class TestResultFilter : IResultFilter
+    {
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            Debug.WriteLine("before");
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            Debug.WriteLine("after");
+        }
+    }
+}
---
+++ WebApplication2/TestResultFilterAsync.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class TestResultFilterAsync : IAsyncResultFilter
+    {
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            Debug.WriteLine("client did not receive response yet");
+
+            await next();
+
+            Debug.WriteLine("client has received the response...");
+        }
+    }
+}
